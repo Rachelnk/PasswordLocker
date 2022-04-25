58ab08c814c3768e7dfc546ca7c9ac2f6806beea
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-25 22:09:10 +0300

    Create User class.

--- passlock.py
+++ passlock.py
@@ -1,3 +1,11 @@
 import random
 import string
 import pyperclip
+
+global users_list 
+# users list is variable that is accessible throughout the program
+
+class User:
+        '''Class to create user accounts and save user information'''
+        users_list = []
+
