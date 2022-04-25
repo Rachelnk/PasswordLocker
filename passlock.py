720dd3bfa00f360d6048e2013def71cd316df6a7
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-25 22:17:04 +0300

    Create instance variables in __init__ method.

--- passlock.py
+++ passlock.py
@@ -6,6 +6,15 @@ global users_list
 # users list is variable that is accessible throughout the program
 
 class User:
-        '''Class to create user accounts and save user information'''
+        '''Class to create user accounts and save user information and generates new instances of user
+        '''
         users_list = []
+        def __init__(self, first_name, last_name, password):
+          '''
+          Method to define the properties that each user objects will hold.
+          '''
+          self.first_name = first_name
+          self.last_name = last_name
+          self.password = password
+          
 
