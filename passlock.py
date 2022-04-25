c4264caceecddef6d98042db10883c883ddd42a7
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-25 22:22:14 +0300

    Create save_newuser function to save a newly created user interface.

--- passlock.py
+++ passlock.py
@@ -16,5 +16,10 @@ class User:
           self.first_name = first_name
           self.last_name = last_name
           self.password = password
+
+        def save_newuser(self):
           
+          User.users_list.append(self)
+
+
 
