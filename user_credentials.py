e59bafb9dd9d04f7cb02c38d13da276f7f66e153
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-26 01:25:10 +0300

    Correct indentation.

--- user_credentials.py
+++ user_credentials.py
@@ -25,34 +25,36 @@ class User:
           User.users_list.append(self)
           
 class Credential:
-  '''
-  Credentials class to create user credentials, generate passowrds and save passwords. 
-  '''
-  credentials_list= []
-  user_credentials_list = []
+        '''
+        Credentials class to create user credentials, generate passowrds and save passwords. 
+        '''
+        credentials_list= []
+        user_credentials_list = []
 
-  @classmethod
-  def check_user(cls, first_name, password):
-    '''
-    Function to check if the name and password provided matches those in the users_list.
-    '''
-    existing_user = ''
-    # declare  variable of type string
-    for user in User.users_list:
-            if (user.firstname == first_name and user.password == password):
-              existing_user = user.first_name
-    return existing_user
+        @classmethod
+        def check_user(cls, first_name, password):
+                '''
+                Function to check if the name and password provided matches those in the users_list.
+                '''
+                existing_user = ''
+                # declare  variable of type string
+                for user in User.users_list:
+                        if (user.firstname == first_name and user.password == password):
+                          existing_user = user.first_name
+                return existing_user
 
-  def __init__(self,username, website_name, account_name, password):
-           self.username = username
-           self.website_name = website_name
-           self.account_name = account_name
-           self.password = password
+        def __init__(self,username, website_name, account_name, password):
+                self.username = username
+                self.website_name = website_name
+                self.account_name = account_name
+                self.password = password
 
-  def save_credentials(self):
-    '''
-    Method to save user's credentials
-    '''
+        def save_credentials(self):
+              '''
+              Method to save user's credentials
+              '''
+              Credential.credentials_list.append(self)
+
 
 
           
