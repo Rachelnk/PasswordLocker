a3ecf4bcfcd6c923f43d001f7d254bd8b43e0c54
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-26 06:04:49 +0300

    Create a method to create a new user account.

--- passlock.py
+++ passlock.py
@@ -2,3 +2,11 @@
 #! /usr/bin/env python3
 import pyperclip
 from user_credentials import User, Credential
+
+def create_user(fname,lname,password):
+        '''
+        Function to create a new user account
+        '''
+        new_user = User(fname,lname,password)
+        return new_user
+
