d2ad7cfc5f8ec94b4e437dd34a5713b9ae63a94e
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-26 12:36:48 +0300

    Create test__init__ method to test if the object is initialized properly.

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -1,3 +1,26 @@
 import unittest
 import pyperclip
 from user_credentials import User, Credential
+
+class TestUser(unittest.TestCase):
+        '''
+        A test class that defines test cases for the user class behaviors.
+
+        Args:
+            unittest:TestCase TestCase class that helps in creating test cases.
+        '''
+        def setUp(self):
+                '''
+                Function to create a user account before each test
+                '''
+                self.new_user = User('Rachel', 'Kiarie', 'pswd123')
+        def test__init__(self):
+                '''
+                test_init test case to test if the object is initialized properly
+                '''
+                self.assertEqual(self.new_user.first_name, 'Ray')
+                self.assertEqual(self.new_user.last_name,'Kiarie')
+                self.assertEqual(self.new_user.password,'pswd123')
+
+
+
