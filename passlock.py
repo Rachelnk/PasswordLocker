bd0e9e8ca808d5fb204ec3f7d5550ab7ea87cfe3
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-26 06:14:19 +0300

    Create a function that automatically generates a password.

--- passlock.py
+++ passlock.py
@@ -5,15 +5,29 @@ from user_credentials import User, Credential
 
 def create_user(fname,lname,password):
         '''
-        Function to create a new user account
+        A function to create a new user account
         '''
         new_user = User(fname,lname,password)
         return new_user
 
 def save_newuser(user):
-      '''
-      A function to save a new user account
-      '''
-      User.save_newuser(user)
+        '''
+        A function to save a new user account
+        '''
+        User.save_newuser(user)
+
+def verify_user(first_name,password):
+        '''
+        A function that checks if the users exists
+        '''
+        checking_user = Credential.check_user(first_name,password)
+        return checking_user
+
+def generate_password():
+        '''
+        A function to generate a password automatically
+        '''
+        generate_userpassword = Credential.generate_password()
+        return generate_userpassword
 
 
